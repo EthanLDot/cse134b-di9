fb2462bd09614735596999a9d5c3df3503091d70
EthanLDot <ethanlee2337@gmail.com>
2025-11-23 19:57:09 -0800

    First commit

---
+++ my-component.js
@@ -0,0 +1,44 @@
+import { getMyComponentCSS } from './my-component-css.js';
+class MyRide extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        // HTML and inline style as template literals for easy modification
+        const style = document.createElement('style');
+        style.textContent = getMyComponentCSS();
+
+        const year = this.getAttribute('year') !== "undefined" ? this.getAttribute('year') : 'Unknown Year';
+        const make = this.getAttribute('make') !== "undefined" ? this.getAttribute('make') : 'Unknown Make';
+        const model = this.getAttribute('model') !== "undefined" ? this.getAttribute('model') : 'Unknown Model';
+        const deets = this.getAttribute('deets') !== "undefined" ? this.getAttribute('deets') : 'Older picture. Nothing to say here!';
+        const imageUrl = this.getAttribute('image-url') || '';
+
+        // Template for component content
+        this.innerHTML = '';
+        this.appendChild(style);
+        this.innerHTML += `
+            <div class="simple-card">
+                <hgroup>
+                <p class="year">${year}</p>
+                <h2 class="model">${make} ${model}</h2>
+                </hgroup>
+                <p class="deets">${deets}</p>
+                <img src="${imageUrl}" alt="${make} ${model}">
+            </div>
+        `;
+        this.querySelector('.simple-card').addEventListener('click', () => this.bubbleUp());
+
+    }
+    bubbleUp() {
+        const event = new CustomEvent('highlight', {
+            bubbles: true
+        });
+        this.dispatchEvent(event);
+    }
+}
+
+// Define the custom element
+console.log("custom element defined");
+customElements.define('my-ride', MyRide);
